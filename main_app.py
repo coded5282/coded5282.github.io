9a48511f342a98f05a609ab7ad51ec6d95b20c9b
Ed Chen <edjchen@hyperturing2.stanford.edu>
2022-09-25 01:47:58 -0700

    Created basic version of publishable website

--- main_app.py
+++ main_app.py
@@ -5,5 +5,17 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route("/index.html")
+def indexa():
+    return render_template('index.html')
+
+@app.route("/about.html")
+def about():
+    return render_template('about.html')
+
+@app.route("/publications.html")
+def publications():
+    return render_template('publications.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
